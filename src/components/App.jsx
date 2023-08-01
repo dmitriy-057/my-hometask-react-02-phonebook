23031aa64cc2d7ac01579cbd185117eb42bc6c4b
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-08-01 23:32:02 +0300

    add formik

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,40 @@
 import React, { Component } from "react";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { nanoid } from 'nanoid';
+// import * as yup from 'yup';
+
+// const schema = yup.object().shape({
+//   name: yup.string().min(4).max(6).required(),
+// });
 
 export class App extends Component {
   state = {
     contacts: [],
     name: ''
   }
+  handleSubmit(values, {resetForm}) {
+    console.log(values);
+    resetForm();
+  }
   render() {
     return (
-      <div>
-        <input
+      <div className="container">
+      <Formik 
+        initialValues={this.state} 
+        // validationSchema={schema}
+        onSubmit={this.handleSubmit}>
+        <Form autoComplete="off">
+        <label htmlFor="name">
+          Name
+          <Field
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
+          />
+          {/* <ErrorMessage name="name" component='div'/> */}
+        </label>
+        <button type="submit">Add contact</button>
+        </Form>
+      </Formik>
       </div>
     );
   }
