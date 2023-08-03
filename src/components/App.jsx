418d109c9015e7175132c95243a24299463cf8c2
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-08-03 23:36:22 +0300

    ...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,23 @@
 import React, { Component } from "react";
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import { nanoid } from 'nanoid';
-// import * as yup from 'yup';
-
-// const schema = yup.object().shape({
-//   name: yup.string().min(4).max(6).required(),
-// });
 
 export class App extends Component {
   state = {
     contacts: [],
     name: ''
   }
+  
   handleSubmit(values, {resetForm}) {
     console.log(values);
     resetForm();
   }
+
   render() {
     return (
       <div className="container">
       <Formik 
         initialValues={this.state} 
-        // validationSchema={schema}
         onSubmit={this.handleSubmit}>
         <Form autoComplete="off">
         <label htmlFor="name">
@@ -30,7 +26,6 @@ export class App extends Component {
           type="text"
           name="name"
           />
-          {/* <ErrorMessage name="name" component='div'/> */}
         </label>
         <button type="submit">Add contact</button>
         </Form>
